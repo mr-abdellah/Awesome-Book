0d8610eb250b3ebe4688cdf4378c8c35ee5060a5
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-24 17:46:19 +0100

    fix local storage issues and fix linters issues

--- script.js
+++ script.js
@@ -1,71 +1,87 @@
+let myBooks = [];
+let storage = [];
+
+const booksContainer = document.querySelector('.books-container');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const addBtn = document.querySelector('#add');
+
+storage = JSON.parse(localStorage.getItem('books')) || [];
+
 class Books {
-  constructor (title, author) {
-      this.title = title;
-      this.author = author;
-  }
-};
-
-class Book {
-  static displayBooks() {
-      let booksList = [];
-
-      const books = booksList;
-
-      books.forEach((book) => Book.addBook(book));
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 
-  static addBook(book) {
+  add() {
+    this.add = localStorage.setItem('books', JSON.stringify(myBooks));
+  }
 
-      // CREATE ELEMENTS
-
-      const booksContainer = document.querySelector('.books-container');
-
-      const bookContainer = document.createElement('li');
-      const bookTitle = document.createElement('h2');
-      const bookAuthor = document.createElement('p');
-      const deleteButton = document.createElement('button');
-
-      // GIVE EACH ELEMNT A CLASS
-  
-      bookContainer.classList.add('bookContainer');
-      bookTitle.classList.add('bookTitle');
-      bookAuthor.classList.add('bookAuthor');
-      deleteButton.classList.add('delete');
-
-      // APPEND ELEMENTS
-  
-      booksContainer.appendChild(bookContainer);
-      bookContainer.appendChild(bookTitle);
-      bookContainer.appendChild(bookAuthor);
-      bookContainer.appendChild(deleteButton);
-
-      // CHANGE ELEMENTS TEXT CONTENT 
-
-      bookTitle.textContent = book.title;
-      bookAuthor.textContent = `Author: ${book.author}`;
-      deleteButton.textContent = 'Delete';
-
-      // Delete Book
-      deleteButton.addEventListener('click', ()=> {
-          bookContainer.remove();
-      })
+  remove() {
+    myBooks = myBooks.filter((element) => element !== this);
+    localStorage.setItem('books', JSON.stringify(myBooks));
   }
 
   static emptyField() {
-      document.querySelector('#title').value = '';
-      document.querySelector('#author').value = '';
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
   }
 }
 
-// Display Books
+function addBook(book, title, author) {
+  book.title = title;
+  book.author = author;
 
-document.addEventListener('DOMContentLoaded', Book.displayBooks);
+  const bookContainer = document.createElement('div');
+  const bookInfo = document.createElement('h2');
+  const deleteButton = document.createElement('button');
 
-document.getElementById('add').addEventListener('click', (e) => {
-    e.preventDefault();
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const newBook = new Books(title, author);
-    Book.addBook(newBook);
-    Book.emptyField();
-})
+  bookContainer.classList.add('bookContainer');
+  bookInfo.classList.add('bookTitle');
+  deleteButton.classList.add('delete');
+
+  booksContainer.appendChild(bookContainer);
+  bookContainer.appendChild(bookInfo);
+  bookContainer.appendChild(deleteButton);
+
+  bookInfo.textContent = ` ${title} by ${author}`;
+  deleteButton.textContent = 'Delete';
+
+  deleteButton.addEventListener('click', (event) => {
+    event.target.parentNode.remove();
+    book.remove();
+  });
+
+  const ul = booksContainer.style;
+  const li = bookContainer.style;
+  bookInfo.style.marginLeft = '2%';
+  bookInfo.style.whiteSpace = 'nowrap';
+  ul.width = '75%';
+  ul.display = 'flex';
+  ul.flexDirection = 'column';
+  li.width = '100%';
+  li.display = 'flex';
+  li.justifyContent = 'space-between';
+  li.alignItems = 'center';
+  li.marginTop = '2%';
+  li.marginBottom = '2%';
+  li.border = '2px solid black';
+  deleteButton.style.marginRight = '2%';
+}
+
+for (let i = 0; i < storage.length; i += 1) {
+  const book = new Books();
+  book.title = storage[i].title;
+  book.author = storage[i].author;
+  myBooks.push(book);
+  addBook(myBooks[i], myBooks[i].title, myBooks[i].author);
+}
+
+addBtn.addEventListener('click', () => {
+  const book = new Books();
+  myBooks.push(book);
+  addBook(book, title.value, author.value);
+  book.add();
+  Books.emptyField();
+});
